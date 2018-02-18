130b22a81f09ddb639af451af7ef6b16e84b0ec4
iloveseoi <rlaehdwn332@nate.com>
2018-02-19 06:37:04 +0900

    5

---
+++ 5/5_solcode.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define loop(i,n) for(int i=0;i<n;i++)
+#define loopx(i,x,n) for(int i=x;i<n;i++)
+
+int comb[1000][1000];
+int main()
+{
+	int tc;
+	cin >> tc;
+	while (tc--) {
+		int n;
+		cin >> n;
+		cout << (n % 2 ? "Buddy" : "Once") << endl;
+	}
+}
