ba464b5823f18e8dce595665dc1075d60e98b460
iloveseoi <rlaehdwn332@nate.com>
2018-02-19 06:36:15 +0900

    1

---
+++ 1/1_solcode.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+using namespace std;
+
+int main() {
+	printf("FINGERTIP\n");
+	printf("umji's card 1998y 8m 19d\n");
+	printf("신비의카드 1998년 6월 3일\n");
+	printf("유주의카드 1997년 10월 4일\n");
+	printf("은하의카드 1997년 5월 30일\n");
+	printf("소원의카드 1995년 12월 7일\n");
+	printf("예린의카드 1996년 8월 19일\n");
+	return 0;
+}
